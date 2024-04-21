984d3cdecb0e8d4c17a4cb4400988acf0480d36e
Diego <dl7designer@gmail.com>
2024-04-21 17:45:32 -0300

    renderizacao do card

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,18 @@
+
+import { Grid } from "@mui/material"
+import "../App.css"
+import CardMemory from "../components/CardMemory"
+import apple from "/apple_level_One.png"
+import type { OperationType } from "../store/modules/operationSlice"
+
+const cards: OperationType[] = []
+
+
 const Home = () => {
     return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
+        <Grid container>
+            <CardMemory value={cards.map((item) => item.firstCard)} operation={'+'} figure={apple} />
+        </Grid>
     )
 }
 
