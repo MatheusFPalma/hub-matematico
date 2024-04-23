1c49fa241165e45ad55dac86b91e0587c2b17f3c
Diego <dl7designer@gmail.com>
2024-04-23 13:25:34 -0300

    alteracao no challengeLevel

--- src/pages/ChallengeLevel.tsx
+++ src/pages/ChallengeLevel.tsx
@@ -6,7 +6,7 @@ import logoMatic from "/logoMatic.png"
 import "../App.css"
 import { useState } from "react"
 import { useAppDispatch } from "../store/hooks"
-import { getOperationOne, getOperationTwo } from "../store/modules/operation.slice"
+import { getOperationOne } from "../store/modules/operation.slice"
 import { useNavigate } from "react-router-dom"
 
 const ChallengeLevel = () => {
@@ -29,6 +29,7 @@ const ChallengeLevel = () => {
                 break;
             case 'Difícil':
                 setCurrentLevel('Difícil')
+                dispatch(getOperationOne('divisao'))
                 navigate('/cards')
                 break;
         }
@@ -38,9 +39,9 @@ const ChallengeLevel = () => {
         <Grid container sx={{ flexDirection: 'column' }}>
             <Grid item sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: "center" }}>
                 <img style={{ width: '140px', height: '150px', marginBottom: '10px' }} src={logoMatic} alt='calculadora' />
-                <Typography >Selecione seu nível de dificuldade</Typography>
+                <Typography sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', textAlign: 'center', fontFamily: 'Fredoka, sans-serif', fontSize: '25.41px', fontWeight: 600, color: '#426141' }}>Selecione seu nível de dificuldade</Typography>
             </Grid>
-            <Grid item sx={{ marginTop: '30px', display: 'flex', flexDirection: 'column', gap: "20px" }}>
+            <Grid item sx={{ marginTop: '30px', display: 'flex', flexDirection: 'column', gap: "20px", justifyContent: 'center', alignItems: "center" }}>
                 <button onClick={() => generateOperation('Fácil')} className="buttonChallenger" value={currentLevel}>Fácil <img src={levelEasy} alt='levelEasy' /></button>
                 <button onClick={() => generateOperation('Médio')} className="buttonChallenger" value={currentLevel}>Médio <img src={levelMiddle} alt='levelMiddle' /></button>
                 <button onClick={() => generateOperation('Difícil')} className="buttonChallenger" value={currentLevel}>Difícil <img src={levelHard} alt='levelHard' /></button>
