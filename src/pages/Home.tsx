5d644377cef16698a32d2f63a577a7142034b875
Diego <dl7designer@gmail.com>
2024-04-23 13:25:13 -0300

    refatoracao nos states

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,10 @@ import { useAppDispatch, useAppSelector } from "../store/hooks"
 import { useEffect, useState } from "react"
 import signalIqual from "/signal_iqual.png"
 import CardResult from "../components/CardResult"
+import { CardType, getCards } from "../store/modules/cards.slice"
+import { v4 as createUuid } from "uuid"
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 
 const Home = () => {
     const isOnHomePage = window.location.pathname === "/cards";
@@ -18,28 +22,47 @@ const Home = () => {
 
     const dispatch = useAppDispatch()
     const operationRedux = useAppSelector((state) => state.operations)
+    const cardsRedux = useAppSelector((state) => state.cards)
     const [resultEquation, setResultEquation] = useState<number | null>(null)
-    const [renderCards, setRenderCards] = useState<number[]>([])
+    const [renderCards, setRenderCards] = useState<CardType[]>([])
     const [renderOperation, setOperation] = useState<string>('')
     const [statement, setStatement] = useState<string>('')
 
     const handleStatement = () => {
-        const firstCard = renderCards[0];
-        const secondCard = renderCards[1];
-        const targetValue = firstCard + secondCard;
-        const operationText = operationRedux.operationLevel === "adicao" ? "somatória" : operationRedux.operationLevel === "multiplicacao" ? "multiplicação" : "";
+        if (renderCards.length >= 2) {
+            const firstCard = renderCards[0].numberCard;
+            const secondCard = renderCards[1].numberCard;
+            const targetValue = firstCard + secondCard;
+            const operationText = operationRedux.operationLevel === "adicao" ? "somatória" : operationRedux.operationLevel === "multiplicacao" ? "multiplicação" : "";
 
-        setStatement(`Selecione os cards cuja ${operationText} de maçãs resultem em ${targetValue} unidades.`);
+            setStatement(`Selecione os cards cuja ${operationText} de maçãs resultem em ${targetValue} unidades.`);
+        }
+        else {
+            setStatement("Aguarde enquanto os cartões estão sendo gerados ...")
+        }
         // setResultEquation(targetValue);
     };
-    function handleCards(): number[] {
-        let cards: number[] = []
-        for (let i = 1; i < 11; i++) {
-            const randomNumber = Math.floor(Math.random() * 10)
-            cards.push(randomNumber);
+
+
+    const handleNumbers = () => {
+        const newCards: CardType[] = [];
+
+        for (let i = 1; i <= 10; i++) {
+            const randomNumber = Math.floor(Math.random() * 10);
+            const cardId = createUuid();
+
+            const newCard: CardType = {
+                cardId: cardId,
+                numberCard: randomNumber,
+                operation: renderOperation,
+                img: apple
+            };
+            newCards.push(newCard);
         }
-        return cards
-    }
+        dispatch(getCards(newCards));
+        return newCards
+    };
+
     const changeOperation = () => {
         switch (operationRedux.operationLevel) {
             case 'adicao':
@@ -61,10 +84,9 @@ const Home = () => {
     }
 
     useEffect(() => {
-        const resultCards = handleCards()
+        const resultCards = handleNumbers()
         changeOperation()
         setRenderCards(resultCards)
-        handleStatement()
     }, [])
 
     useEffect(() => {
@@ -78,9 +100,9 @@ const Home = () => {
             </Grid>
             <Box className={'backgroundScreen'} sx={{ justifyContent: 'center', alignItems: 'center', display: 'flow', flexFlow: 'column' }}>
                 <Grid item xs={12} sm={6} md={4} sx={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap', margin: '40px 40px 10px' }}>
-                    {renderCards.map((item, index) => (
-                        <CardMemory key={index} value={item} operation={renderOperation} figure={apple} />
-                    ))}
+                    {renderCards.length > 0 ? renderCards.map((item) => (
+                        <CardMemory key={item.cardId} value={item.numberCard} operation={renderOperation} figure={apple} />
+                    )) : <Spin indicator={<LoadingOutlined style={{ fontSize: 24 }} spin />} />}
                 </Grid>
                 <Grid item sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                     <img style={{ width: '20px', height: '20px', marginRight: '10px' }} src={signalIqual} alt='signalIqual' />
