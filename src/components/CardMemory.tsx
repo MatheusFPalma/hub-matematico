1550e838cc8f2658facb4e33f1e5d4cc725448be
Diego <dl7designer@gmail.com>
2024-04-24 00:36:55 -0300

    alteracao no cardMemory

--- src/components/CardMemory.tsx
+++ src/components/CardMemory.tsx
@@ -2,12 +2,13 @@ import { Box, Paper, Typography, useTheme } from '@mui/material'
 import "../App.css";
 
 interface CardMemoryProps {
-    value: number | undefined | React.ReactNode;
+    value: number | undefined
     figure: string
     operation?: React.ReactNode
+    action: (value: number | undefined) => void
 }
 
-const CardMemory: React.FC<CardMemoryProps> = ({ value, figure, operation }) => {
+const CardMemory: React.FC<CardMemoryProps> = ({ value, figure, operation, action }) => {
     const theme = useTheme()
 
     return (
@@ -17,8 +18,8 @@ const CardMemory: React.FC<CardMemoryProps> = ({ value, figure, operation }) => 
                 flexWrap: 'wrap',
                 '& > :not(style)': {
                     m: 1,
-                    width: 68,
-                    height: 108,
+                    width: 78,
+                    height: 118,
                     borderRadius: 4,
                     backgroundColor: theme.palette.primary.main,
                     color: theme.palette.secondary.main
@@ -26,8 +27,10 @@ const CardMemory: React.FC<CardMemoryProps> = ({ value, figure, operation }) => 
             }}
         >
             <Paper sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <Typography style={{ width: '40px', height: '40px', justifyContent: 'center', display: 'flex', alignItems: 'center', fontFamily: 'Fredoka', fontSize: '24px', fontWeight: 600 }} className='styleNumberCard'>{value} <span style={{ display: 'flex', paddingLeft: '5px', alignItems: 'center', height: '40px', fontSize: '18px' }}>{operation}</span></Typography>
-                <img style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end', width: '38px', height: '33px' }} src={figure} alt='apple' />
+                <button style={{ border: 'none', backgroundColor: 'transparent', cursor: 'pointer' }} onClick={() => action(value)}>
+                    <Typography style={{ width: '40px', height: '40px', justifyContent: 'center', display: 'flex', alignItems: 'center', fontFamily: 'Fredoka', fontSize: '38px', fontWeight: 600, color: theme.palette.secondary.main }} className='styleNumberCard'>{value} <span style={{ display: 'flex', paddingLeft: '5px', alignItems: 'center', height: '40px', width: '25px', fontSize: '40px' }}>{operation}</span></Typography>
+                    <img style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end', width: '40px', height: '38px' }} src={figure} alt='apple' />
+                </button>
             </Paper>
         </Box>
     )
