d2dd9707ccbd868038a4113e6cd6e26ffaa3fa0f
Diego <dl7designer@gmail.com>
2024-04-24 00:38:01 -0300

    alteracao no cardResult

--- src/components/CardResult.tsx
+++ src/components/CardResult.tsx
@@ -1,7 +1,9 @@
 import { Box, Paper, Typography, useTheme } from "@mui/material"
+import { useAppSelector } from "../store/hooks"
+import { useEffect, useState } from "react"
 
 interface CardResultProps {
-    value: number | null
+    value: number | undefined
 }
 
 const CardResult: React.FC<CardResultProps> = ({ value }) => {
@@ -23,7 +25,7 @@ const CardResult: React.FC<CardResultProps> = ({ value }) => {
             }}
         >
             <Paper sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <Typography style={{ width: '45px', height: '25px', justifyContent: 'center', display: 'flex', alignItems: 'center', fontFamily: 'Fredoka', fontSize: '20px', fontWeight: 600 }} className='styleNumberCard'>{value}</Typography>
+                <Typography style={{ width: '45px', height: '25px', justifyContent: 'center', display: 'flex', alignItems: 'center', fontFamily: 'Fredoka', fontSize: '42px', fontWeight: 600 }} className='styleNumberCard'>{value}</Typography>
             </Paper>
         </Box>
     )
