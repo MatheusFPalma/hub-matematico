94c090840054c7ceafaae51a739c82c0a1cbd639
Diego <dl7designer@gmail.com>
2024-04-24 00:41:42 -0300

    challenge slice incluido no rootReducer

--- src/store/modules/rootReducer.ts
+++ src/store/modules/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import cardsSlice from "./cards.slice";
 import operationSlice from "./operation.slice";
+import challengeSlice from "./challenge.slice";
 
 export default combineReducers({
     cards: cardsSlice,
-    operations: operationSlice
+    operations: operationSlice,
+    challenges: challengeSlice
 })
