59cc09c0347e7499b8f2f4550899ce993deb6400
Diego <dl7designer@gmail.com>
2024-04-25 14:21:37 -0300

    altercao no choice level

--- src/pages/ChoiceLevel.tsx
+++ src/pages/ChoiceLevel.tsx
@@ -42,9 +42,9 @@ const ChoiceLevel = () => {
                 <Typography sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', textAlign: 'center', fontFamily: 'Fredoka, sans-serif', fontSize: '25.41px', fontWeight: 600, color: '#426141' }}>Selecione seu nível de dificuldade</Typography>
             </Grid>
             <Grid item sx={{ marginTop: '30px', display: 'flex', flexDirection: 'column', gap: "20px", justifyContent: 'center', alignItems: "center" }}>
-                <button onClick={() => getLevel('Fácil')} className="buttonChallenger" value={currentLevel}>Fácil <img src={levelEasy} alt='levelEasy' /></button>
-                <button onClick={() => getLevel('Médio')} className="buttonChallenger" value={currentLevel}>Médio <img src={levelMiddle} alt='levelMiddle' /></button>
-                <button onClick={() => getLevel('Difícil')} className="buttonChallenger" value={currentLevel}>Difícil <img src={levelHard} alt='levelHard' /></button>
+                <button onClick={() => getLevel('Fácil')} className="buttonChoiceLevel" value={currentLevel}>Fácil <img src={levelEasy} alt='levelEasy' /></button>
+                <button onClick={() => getLevel('Médio')} className="buttonChoiceLevel" value={currentLevel}>Médio <img src={levelMiddle} alt='levelMiddle' /></button>
+                <button onClick={() => getLevel('Difícil')} className="buttonChoiceLevel" value={currentLevel}>Difícil <img src={levelHard} alt='levelHard' /></button>
             </Grid>
         </Grid>
     )
