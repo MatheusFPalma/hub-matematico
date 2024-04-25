8b61f5040b3dce9cda84e7377de91ab2724a420f
Diego <dl7designer@gmail.com>
2024-04-25 14:18:08 -0300

    add inclusao de capturada de click para alterar cor do card

--- src/components/CardMemory.tsx
+++ src/components/CardMemory.tsx
@@ -1,5 +1,6 @@
 import { Box, Paper, Typography, useTheme } from '@mui/material'
 import "../App.css";
+import { useState } from 'react';
 
 interface CardMemoryProps {
     value: number | undefined
@@ -10,6 +11,13 @@ interface CardMemoryProps {
 
 const CardMemory: React.FC<CardMemoryProps> = ({ value, figure, operation, action }) => {
     const theme = useTheme()
+    const [clicked, setClicked] = useState(false)
+
+    const handleClick = () => {
+        setClicked(!clicked); // Alternar o estado ao clicar no card
+        action(value); // Chamar a função de ação fornecida
+    };
+
 
     return (
         <Box
@@ -21,14 +29,13 @@ const CardMemory: React.FC<CardMemoryProps> = ({ value, figure, operation, actio
                     width: 78,
                     height: 118,
                     borderRadius: 4,
-                    backgroundColor: theme.palette.primary.main,
-                    color: theme.palette.secondary.main
+                    backgroundColor: clicked ? theme.palette.secondary.contrastText : theme.palette.secondary.light,
                 },
             }}
         >
-            <Paper sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <button style={{ border: 'none', backgroundColor: 'transparent', cursor: 'pointer' }} onClick={() => action(value)}>
-                    <Typography style={{ width: '40px', height: '40px', justifyContent: 'center', display: 'flex', alignItems: 'center', fontFamily: 'Fredoka', fontSize: '38px', fontWeight: 600, color: theme.palette.secondary.main }} className='styleNumberCard'>{value} <span style={{ display: 'flex', paddingLeft: '5px', alignItems: 'center', height: '40px', width: '25px', fontSize: '40px' }}>{operation}</span></Typography>
+            <Paper className='styleCard' sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                <button style={{ border: 'none', backgroundColor: 'transparent', cursor: 'pointer' }} onClick={() => handleClick()}>
+                    <Typography style={{ width: '40px', height: '40px', justifyContent: 'center', display: 'flex', alignItems: 'center', fontFamily: 'Fredoka', fontSize: '38px', fontWeight: 600, color: clicked ? '#fff' : theme.palette.secondary.main }}>{value} <span style={{ display: 'flex', paddingLeft: '5px', alignItems: 'center', height: '40px', width: '25px', fontSize: '40px' }}>{operation}</span></Typography>
                     <img style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'flex-end', width: '40px', height: '38px' }} src={figure} alt='apple' />
                 </button>
             </Paper>
