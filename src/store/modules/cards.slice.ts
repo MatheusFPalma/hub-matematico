151c3f3c8308a261fc12c920341b1aa118974a58
Diego <dl7designer@gmail.com>
2024-04-28 00:39:50 -0300

    refatoracao no card slice

--- src/store/modules/cards.slice.ts
+++ src/store/modules/cards.slice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 export interface CardType {
     cardId: string
@@ -9,10 +9,16 @@ export interface CardType {
 
 interface CardState {
     cards: CardType[]
+    // lastSelectedCards: string[]
+    // lastSelectedNumbers: number[];
+    lastSelectedCards: CardType[];
 }
 
 export const initialState: CardState = {
-    cards: []
+    cards: [],
+    lastSelectedCards: [],
+    // lastSelectedNumbers: [],
+
 }
 
 export const cardsSlice = createSlice({
@@ -22,9 +28,37 @@ export const cardsSlice = createSlice({
         getCards: (state, action) => {
             state.cards = action.payload
             return state
-        }
+        },
+        // removeCard: (state, action: PayloadAction<CardType>) => {
+        //     const findCardIdRepeted = action.payload
+        //     const cardIdSelected = state.lastSelectedCards.find(item => item.cardId === findCardIdRepeted.cardId)
+
+        //     if (!cardIdSelected) {
+        //         state.lastSelectedCards.push(findCardIdRepeted)
+        //         return state
+        //     }
+        //     const indexCardIdSelected = state.lastSelectedCards.findIndex((item) => item.cardId === action.payload.cardId)
+        //     state.lastSelectedCards.splice(indexCardIdSelected, 1)
+        //     return state
+        // },
+        // removeShift: (state, action: PayloadAction<CardType>) => {
+        //     // Remove o número da primeira carta clicada, mantendo apenas o último
+        //     state.lastSelectedCards.shift();
+        // },
+        setLastSelectedCard: (state, action: PayloadAction<CardType>) => {
+            state.lastSelectedCards.push(action.payload);
+            return state;
+        },
+        removeLastSelectedCard: (state, action: PayloadAction<CardType>) => {
+            state.lastSelectedCards = state.lastSelectedCards.filter((card) => card.cardId !== action.payload.cardId);
+            return state;
+        },
+        // clearLastSelectedCards: (state) => {
+        //     state.lastSelectedCards = [];
+        //     return state;
+        // }
     }
 })
 
-export const { getCards } = cardsSlice.actions
+export const { getCards, setLastSelectedCard, removeLastSelectedCard } = cardsSlice.actions
 export default cardsSlice.reducer
