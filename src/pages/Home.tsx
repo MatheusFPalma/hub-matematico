170d8ebc6b03d9ef2e7d8eb252ef4fc858a0c29f
Solange Righi <slngvitoria@gmail.com>
2024-04-29 16:34:13 -0300

    criação da page de onboard

---
+++ src/pages/Home.tsx
@@ -0,0 +1,28 @@
+import { Box } from "@mui/material"
+import "../App.css"
+import logoMatic from "/logoMatic.png"
+import CardWelcome from "../components/CardWelcome"
+import girlGlasses from "/girlGlasses.jpg"
+import girl from "/girl.png"
+import boy from "/boy.png"
+
+function Home() {
+  const originalText = "Treine operações matemáticas de um modo divertido"
+
+  return (
+    <Box
+      height={"100vh"}
+      width={"100vw"}
+      display={"flex"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      flexDirection={"column"}
+      bgcolor={"#fbfffb"}
+    >
+      <img src={logoMatic} alt="oi" height={"20%"}></img>
+      <CardWelcome text={originalText} image={girl} />
+    </Box>
+  )
+}
+
+export default Home
