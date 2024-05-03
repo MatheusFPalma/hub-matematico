919968006068fff97326fe7d36f9ac9ad3c468bf
Diego <dl7designer@gmail.com>
2024-05-03 18:59:24 -0300

    refatoracao no challengeSlice

--- src/store/modules/challenge.slice.ts
+++ src/store/modules/challenge.slice.ts
@@ -1,18 +1,54 @@
+// import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+// export interface EquationType {
+//     firstNumber: number;
+//     secondNumber: number;
+//     operation: string | null;
+//     result: number;
+// }
+
+// interface ChallengeState {
+//     historyEquations: EquationType[];
+// }
+
+// export const initialState: ChallengeState = {
+//     historyEquations: []
+// }
+
+// export const challengeSlice = createSlice({
+//     name: 'getHistory',
+//     initialState,
+//     reducers: {
+//         addEquationHistory: (state, action: PayloadAction<EquationType>) => {
+//             state.historyEquations.push(action.payload)
+//             return state
+//         }
+//     }
+// })
+
+// export const { addEquationHistory } = challengeSlice.actions
+// export default challengeSlice.reducer
+
+
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 export interface EquationType {
     firstNumber: number;
     secondNumber: number;
-    operation: string | null;
+    operation: '+' | '-' | 'x' | '÷' | null
     result: number;
 }
 
 interface ChallengeState {
-    historyEquations: EquationType[];
+
+    // historyEquations: EquationType[];
 }
 
-export const initialState: ChallengeState = {
-    historyEquations: []
+export const initialState: EquationType = {
+    firstNumber: 0,
+    secondNumber: 0,
+    operation: null,
+    result: 0,
 }
 
 export const challengeSlice = createSlice({
@@ -20,7 +56,10 @@ export const challengeSlice = createSlice({
     initialState,
     reducers: {
         addEquationHistory: (state, action: PayloadAction<EquationType>) => {
-            state.historyEquations.push(action.payload)
+            state.firstNumber = action.payload.firstNumber
+            state.secondNumber = action.payload.secondNumber
+            state.operation = action.payload.operation
+            state.result = action.payload.result
             return state
         }
     }
