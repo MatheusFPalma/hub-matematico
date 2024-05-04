56c743895c90ae9c7f0f918888d46200630ae2e7
Solange Righi <slngvitoria@gmail.com>
2024-05-04 12:20:49 -0300

    Squashed commit of the following:
    commit 0734a9704e7d4e6948e4256a252d230cbc8cf9db
    Author: Solange Righi <slngvitoria@gmail.com>
    Date:   Sat May 4 11:29:33 2024 -0300
    
        criação da Home/tutorial, edição de direcionamento da choiceLevel pra home, criação de enum pra cores

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,6 +2,8 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import ChoiceLevel from "../pages/ChoiceLevel"
 import PlayRoom from "../pages/PlayRoom"
 import Onboarding from "../pages/Onboarding"
+import Home from "../pages/Home"
+import Tutorial from "../pages/Tutorial"
 
 const router = createBrowserRouter([
   {
@@ -14,18 +16,15 @@ const router = createBrowserRouter([
   },
   {
     path: "/tutorial",
-    element: (
-      <h2>
-        Aqui será a tela de tutorial com os cards dos tutoriais dos 3 níveis de
-        dificuldade O card pode ser um componente que renderiza o tutorial do
-        nível de acordo com o nível que foi escolhido pelo usuário na tela
-        anterior
-      </h2>
-    ),
+    element: <Tutorial />,
   },
   {
     path: "/choiceLevel",
     element: <ChoiceLevel />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
   },
 ])
 
